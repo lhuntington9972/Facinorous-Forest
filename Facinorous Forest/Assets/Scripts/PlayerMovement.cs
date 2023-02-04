158ad1a75193fd55f8103aec3acd4cc57b456aa4
Luke Huntington <lhuntington9972@student.pps.net>
2023-02-04 10:43:15 -0800

    movement/map
    Basic side to side movement + jump (its dogshit) and ground/background. (also added assets from the asset store)

--- Facinorous Forest/Assets/Scripts/PlayerMovement.cs
+++ Facinorous Forest/Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
 
     public Rigidbody2D playerRigidBody;
+    
     
     // Start is called before the first frame update
     void Start()
