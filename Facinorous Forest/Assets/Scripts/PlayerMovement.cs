f25b1cbe36447973177ac3a16b8e616d7e615be3
Luke <lhuntington9972@student.pps.net>
2023-02-06 15:26:05 -0800

    trash movement delete immediately
    it sucks

--- Facinorous Forest/Assets/Scripts/PlayerMovement.cs
+++ Facinorous Forest/Assets/Scripts/PlayerMovement.cs
@@ -7,31 +7,56 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody2D playerRigidBody;
     
+    private float movementSpeed;
+    private float jumpDistance;
+    private bool isJumping;
+    float moveHorizontal;
+    float moveVertical;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidBody = gameObject.GetComponent<Rigidbody2D>();
+
+        movementSpeed = 3;
+        jumpDistance = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        if (Input.GetKeyDown(KeyCode.Space))
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+
+    }
+
+    void FixedUpdate()
+    {
+
+        if(moveHorizontal > 0.1f || moveHorizontal < -0.1f)
         {
-            transform.position = transform.position + new Vector3(0, 1, 0);
+            playerRigidBody.AddForce(new Vector2(moveHorizontal * movementSpeed, 0f), ForceMode2D.Impulse);
+        } 
+
+        if(!isJumping && moveVertical > 0.1f)
+        {
+            playerRigidBody.AddForce(new Vector2(0f, moveVertical * jumpDistance));
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if(collision.gameObject.tag == "Platform")
+        {
+            isJumping = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            transform.position = transform.position + new Vector3(-1, 0, 0);
-        }
+    }
 
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            transform.position = transform.position + new Vector3(1, 0, 0);
-        }
+    void OnTriggerExit2D(Collider2D collision)
+    {
 
     }
 }
