98c8ef8dee172dde381b1c6ee118c964d77c54b6
Luke <lhuntington9972@student.pps.net>
2023-03-10 11:38:39 -0800

    background implemented
    not finished with parallax

---
+++ Facinorous Forest/Assets/Scripts/CameraMover.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+
+    public Transform playerLocation;
+
+    private void FixedUpdate()
+    {
+        this.transform.position = new Vector3(playerLocation.position.x, playerLocation.position.y, -10f);
+    }
+}
